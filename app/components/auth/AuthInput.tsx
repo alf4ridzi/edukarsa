907bdb78d2ddf7e7d3c6ee83f4dc9a220f409156
alf4ridzi <aalalfaridzi9@gmail.com>
2025-12-18 18:01:59 +0700

    add loading

--- app/components/auth/AuthInput.tsx
+++ app/components/auth/AuthInput.tsx
@@ -1,26 +1,47 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, TouchableOpacity } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
 
 type AuthInputProps = {
   icon: ComponentProps<typeof FontAwesome>["name"];
   placeholder?: string;
   secureTextEntry?: boolean;
+  value?: string;
+  onChangeText?: (text: string) => void;
 };
 
 export default function AuthInput({
   icon,
   placeholder,
-  secureTextEntry,
+  secureTextEntry = false,
+  value,
+  onChangeText,
 }: AuthInputProps) {
+  const [hidePassword, setHidePassword] = useState(secureTextEntry);
+
   return (
     <View className="flex-row items-center border-b border-blue-500 mb-6">
       <FontAwesome name={icon} size={18} color="#2563eb" />
+
       <TextInput
+        value={value}
+        onChangeText={onChangeText}
         placeholder={placeholder}
-        secureTextEntry={secureTextEntry}
+        secureTextEntry={hidePassword}
+        autoCapitalize="none"
+        autoCorrect={false}
         className="flex-1 ml-3 py-2 text-base"
       />
+
+      {secureTextEntry && (
+        <TouchableOpacity onPress={() => setHidePassword(!hidePassword)}>
+          <FontAwesome
+            name={hidePassword ? "eye-slash" : "eye"}
+            size={18}
+            color="#2563eb"
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
--- app/screens/auth/LoginScreen.tsx
+++ app/screens/auth/LoginScreen.tsx
@@ -2,10 +2,20 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import AuthInput from "../../components/auth/AuthInput";
 import { AuthStackParamList } from "@/navigation/AuthNavigator";
+import { useState } from "react";
 
 type Props = NativeStackScreenProps<AuthStackParamList, "Login">;
 
 export default function LoginScreen({ navigation }: Props) {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async () => {
+    console.log(identifier);
+    console.log(password);
+  };
+
   return (
     <View className="flex-1 bg-white px-8 justify-center">
       <View className="items-center mb-10">
@@ -18,12 +28,30 @@ export default function LoginScreen({ navigation }: Props) {
         <Text className="text-slate-500 mt-2">Selamat Datang</Text>
       </View>
 
-      <AuthInput icon="user" placeholder="Username / NIS / Email" />
-      <AuthInput icon="lock" placeholder="Password" secureTextEntry />
+      <AuthInput
+        icon="user"
+        placeholder="Username / NIS / Email"
+        value={identifier}
+        onChangeText={setIdentifier}
+      />
 
-      <TouchableOpacity className="bg-blue-500 py-4 rounded-full mt-4">
+      <AuthInput
+        icon="lock"
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity
+        onPress={handleLogin}
+        disabled={loading}
+        className={`bg-blue-500 py-4 rounded-full mt-4 ${
+          loading ? "opacity-60" : ""
+        }`}
+      >
         <Text className="text-center text-white text-lg font-semibold">
-          Login
+          {loading ? "Loading..." : "Login"}
         </Text>
       </TouchableOpacity>
 
